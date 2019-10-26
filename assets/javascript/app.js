148ecb1dce53813720fa01d1c6af6b1865476da1
Miguel Galarraga <miguelangelgalarraga@gmail.com>
2019-10-26 19:39:15 -0400

    the functions added to create the favorite section were deleted, since the first function was not working as desire, also some comments wereedited and added to the .js file, etc

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,11 +8,12 @@ $(document).ready(function () {
     let a1 = 10;
 
 
-    // Function for displaying animals data
+    // Function for displaying animals's buttons
 
-    // Deleting the animals buttons prior to adding new animals buttons
-    // (this is necessary otherwise we will have repeat buttons)
     function addButtons() {
+
+        // Deleting the animals buttons prior to adding new animals buttons
+        // (this is necessary otherwise we will have repeat buttons)
 
         $("#buttons-view").empty();
 
@@ -68,8 +69,10 @@ $(document).ready(function () {
 
     addButtons();
 
+    // function named "engine" that basically perform the AJAX request, contructs the queryURL
 
     $(document).on("click", ".animal", function engine() {
+
         // Grabbing and storing the data-animal property value from the button
 
         let animals2 = $(this).attr("data-animal");
@@ -82,7 +85,8 @@ $(document).ready(function () {
             animals2 + "&api_key=42AnQTx6o2pFP9hsR2BUpy0AcHLw4UEE&limit=" + a1;
 
 
-        // Performing an AJAX request with the queryURL
+        // Performing an AJAX request with the queryURL, and creates the 10 gifts as requested by the user everytime that click 
+        // on one of the button which animal name.
 
         $.ajax({
             url: queryURL,
@@ -107,7 +111,7 @@ $(document).ready(function () {
 
                     var animalDiv = $("<div>");
 
-                    // Creating a paragraph tag with the result item's rating
+                    // Creating a paragraph tag with the result item's rating, title and source.
 
                     var p = $("<p>").text("Rating: " + results[i].rating);
                     var p1 = $("<p>").text("Title: " + results[i].title);
@@ -117,7 +121,7 @@ $(document).ready(function () {
 
                     var animalImage = $("<img>");
 
-                    // Setting the src attributes amd class of the image to a property pulled off the result item
+                    // Setting the src attributes and class of the image to a property pulled off the result item
 
                     let animated = results[i].images.fixed_height.url;
                     let static = results[i].images.fixed_height_still.url;
@@ -128,12 +132,12 @@ $(document).ready(function () {
                     animalImage.attr("static-s", static);
                     animalImage.attr("animate-s", animated);
 
-                    // Appending the paragraph and image tag to the animalDiv
+                    // Appending the paragraphs and image tag to the animalDiv
 
                     animalDiv.append(p, p1, p2);
                     animalDiv.append(animalImage);
 
-                    // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+                    // Prependng the animalDiv to the HTML page in the "#images" div
 
                     $("#images").prepend(animalDiv);
                 }
@@ -142,11 +146,11 @@ $(document).ready(function () {
 
     });
 
-    //Click event on gifs with class of "netflixGiphy" executes pausePlayGifs function 
+    //Click event on gifs with class of "kinematic" executes sDynamic function 
 
     $(document).on("click", ".kinematic", sDynamic);
 
-    //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
+    //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-static"
 
     function sDynamic() {
         var state = $(this).attr("state");
@@ -159,31 +163,7 @@ $(document).ready(function () {
         }
     };
 
-
-    $("#pick").on('click', function pickAnimal3() {
-
-        $(document).on("click", ".kinematic", pickAnimalName);
-
-        function pickAnimalName() {
-            let animalName1 = $(this).attr("src");
-            console.log(animalName1);
-            var animalDiv2 = $("<div>");
-            var animalImage2 = $("<img>");
-            animalImage2.addClass("kinematic1");
-            animalImage2.attr("src", animalName1);
-            animalDiv2.append(animalImage2);
-            $("#favorites").prepend(animalDiv2);
-        };
-    });
-
-    $("#pick1").on('click', function () {
-
-        $(document).on("click", ".kinematic1", pickAnimalName1);
-
-        function pickAnimalName1() {
-            $(this).remove();
-        };
-    });
+    //Function  to re-start the app.
 
     $('#str1').on('click', function rStart() {
         window.location.reload(false);
